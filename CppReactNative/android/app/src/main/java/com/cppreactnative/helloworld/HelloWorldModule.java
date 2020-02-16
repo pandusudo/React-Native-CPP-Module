5ca6426de844c2028f099f8b71a0ae9c2c13e68c
pandusudo <pandusudo@gmail.com>
2020-02-16 18:20:06 +0700

    initial commit

---
+++ CppReactNative/android/app/src/main/java/com/cppreactnative/helloworld/HelloWorldModule.java
@@ -0,0 +1,33 @@
+package com.cppreactnative.helloworld;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class HelloWorldModule extends ReactContextBaseJavaModule {
+
+    // Add the following lines
+    private HelloWorld cppApi; // instance var for our cppApi
+
+    static {
+        System.loadLibrary("helloworld"); // load the "helloworld" JNI module
+    }
+
+    public HelloWorldModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        cppApi = HelloWorld.create(); // create a new instance of our cppApi
+    }
+
+    @Override
+    public String getName() {
+        return "HelloWorld";
+    }
+
+    @ReactMethod
+    public void sayHello(Promise promise) {
+        // call the "getHelloWorld()" method on our C++ class and get the results.
+        String myString = cppApi.getHelloWorld();
+        promise.resolve(myString);
+    }
+}
